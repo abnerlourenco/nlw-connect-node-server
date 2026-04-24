e81f8b3af2650b1370824a838a94d72b3649bfd0
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2026-04-24 20:01:46 +0000

    Ajuste: adiciona configuração de verificação de prontidão e tratamento de erro para a conexão Redis

--- src/redis/client.ts
+++ src/redis/client.ts
@@ -1,4 +1,10 @@
 import { Redis } from 'ioredis'
 import { env } from '../env.ts'
 
-export const redis = new Redis(env.REDIS_URL)
+export const redis = new Redis(env.REDIS_URL, {
+  enableReadyCheck: false,
+})
+
+redis.on('error', (err) => {
+  console.error('Redis Connection Error:', err.message);
+});
